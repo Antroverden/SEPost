2b42fe8c9297f144e2106dcbf68295d28b5a59f1
Antroverden <Antroverden@yandex.ru>
2023-08-17 14:18:05 +0300

    fix: unused class
    (cherry picked from commit 37da726a09c73c9b5949bec75de7a4424224a2f4)

--- src/main/java/com/skyeng/SEPost/repository/PostOfficeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.skyeng.SEPost.repository;
-
-import com.skyeng.SEPost.entity.PostOffice;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostOfficeRepository extends JpaRepository<PostOffice, Integer> {
-}
